a0b93066ec1eb40e3ce422a2dcd0e7e261576d8f
Matt Wittmann <matt@mattwittmann.com>
2017-09-10 18:18:35 -0500

    Have it display a random number, picked on server start-up

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
-import "fmt"
-import "log"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	randomValue := rand.Int()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, world!")
+		fmt.Fprint(w, "Value: ", randomValue)
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
